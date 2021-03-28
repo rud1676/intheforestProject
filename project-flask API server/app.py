8999d56112a883970b055573d90c765bd864fe1a
rud1676 <rud167637@gmail.com>
2021-03-29 02:08:15 +0900

    NetworkConnection sysmon 설정 변경(프로그램 파일 경로 추가) + api 만들다가 실패(multi term 지원여부고민)

--- project-flask API server/app.py
+++ project-flask API server/app.py
@@ -4,6 +4,7 @@ from flask_cors import CORS
 from processCreate import ProcessCreate
 from Employee import employee
 from driverload import driverload
+from networkConnection import networkConnection
 
 
 app = Flask(__name__)
@@ -19,6 +20,7 @@ api = Api(app,
 api.add_namespace(ProcessCreate, '/processCreate')
 api.add_namespace(employee, '/employee')
 api.add_namespace(driverload, '/driverload')
+api.add_namespace(networkConnection, '/networkConnection')
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=80)
---
+++ project-flask API server/networkConnection.py
@@ -0,0 +1,35 @@
+# flask모듈을 import
+from flask import request
+from flask_restx import Resource, Namespace
+import datetime  # UTC로 나온 시간을 한국시간으로 맞추기 위함
+import time
+from elasticsearch import Elasticsearch  # elk서버와의 통신
+networkConnection = Namespace(name='networkConnection',
+                              description="About networkConnection 이벤트")
+es = Elasticsearch('34.64.140.231:9200')
+
+
+@networkConnection.route("/alert")
+class alert(Resource):
+    def get(self):
+        """Alert기능이 꺼졋는지 켜졋는지 얻어옵니다."""
+        body = {
+            "size": 10000,
+            "aggs": {
+                "stations": {
+                    "terms": {
+                        "field": "data.win.eventdata.image"
+                    }
+                }
+            }
+        }  # 집계를 위한 쿼리
+        for r in es.search(index=".opendistro-alerting-config")["hits"]["hits"]:
+            s = r["_source"]
+            if s.get("monitor") != None:
+                if s["monitor"]["name"] == "networkConnection event check":
+                    v = s["monitor"]["enabled"]
+        if v == True:
+            returnValue = "green"
+        else:
+            returnValue = "error"
+        return returnValue
